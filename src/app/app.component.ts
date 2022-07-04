f511bce01f5512942cd5a701e7c90bc346d7236d
sachinsrivastava17 <92143412+sachinsrivastava17@users.noreply.github.com>
2022-07-04 18:11:05 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-app';
+  constructor(){
+    setTimeout(() => {
+      this.title = "Sachin Srivastava"
+    }, 2000);
+  }
 }
